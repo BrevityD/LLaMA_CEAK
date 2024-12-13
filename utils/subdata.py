df815a9bacb7f9412dc38582b312d411285a8a50
dzj <brevity_d@mail.ustc.edu.cn>
2024-12-13 14:22:09 +0800

    feat: filter train data by lce

---
+++ utils/subdata.py
@@ -0,0 +1,14 @@
+"""This script filters molecules based on their LCE. 
+Only molecules with an LCE greater than 1 are selected, as those with an LCE less than 
+or equal to 1 are considered potentially ineffective or unsuitable for further analysis."""
+import pandas as pd
+
+origin_file = "data/ceak_datasets.csv"
+target_file = "data/ceak_datasets_sub.csv"
+
+df = pd.read_csv(origin_file)
+
+
+filtered_df = df[df['lce']>1]
+
+filtered_df.to_csv(target_file, index=False)
